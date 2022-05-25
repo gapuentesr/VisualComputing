28984848e1a791de0cd42cd63eb0657d5c27c4b4
gapuentesr <gapuentesr@users.noreply.github.com>
2022-05-25 13:58:54 +0000

    deploy: d11206be17d09997e216db94558c8684b8b5372d

--- sketches/sceneTrees/3dbrush.js
+++ sketches/sceneTrees/3dbrush.js
@@ -4,44 +4,42 @@ let pose;
 let skeleton;
 
 function setup() {
-  createCanvas(640, 480, WEBGL);
+  createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.hide();
-
   poseNet = ml5.poseNet(video, modelLoaded);
   poseNet.on('pose', gotPoses);
 }
 
-function gotPoses(poses){
-  //console.log(poses);
-  if (poses.length > 0){
+function gotPoses(poses) {
+  if (poses.length > 0) {
     pose = poses[0].pose;
     skeleton = poses[0].skeleton;
   }
 }
 
-function modelLoaded(){
-  console.log('poseNet está listo')
+function modelLoaded() {
+  console.log('Modelo PoseNet cargado y listo');
 }
 
 function draw() {
-  image(video, -320, -270);
+  image(video, 0, 0);
 
-  if (pose){
-    for (let i = 0; i < pose.keypoints.length; i++){
-      let x = pose.keypoints[i].position.x  - 320;
-      let y = pose.keypoints[i].position.y - 270;
-      fill(0, 255, 0);
+  if (pose) {
+
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      fill(255, 0, 0);
       ellipse(x, y, 10, 10);
     }
 
-    for (let i = 0; i < skeleton.length; i++){
-      let a = skeleton[i][0] -320;
-      let b = skeleton[i][1] -270;
+    for (let i = 0; i < skeleton.length; i++) {
+      let a = skeleton[i][0];
+      let b = skeleton[i][1];
       strokeWeight(2);
-      stroke(255);
+      stroke(0);
       line(a.position.x, a.position.y, b.position.x, b.position.y);
     }
-    
   }
 }

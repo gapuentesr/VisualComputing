83901836d02866a0e0af4e2e253efc2943c0d554
gapuentesr <gapuentesr@users.noreply.github.com>
2022-06-28 11:57:38 +0000

    deploy: 08463fade0fc6af90504db9e04ee445e9c0916f4

--- sketches/shaders/pixelator/pixelator.js
+++ sketches/shaders/pixelator/pixelator.js
@@ -1,21 +1,25 @@
-let lumaShader;
+let pixelatorShader;
 let img;
 
 function preload() {
-  lumaShader = readShader('/VisualComputing/sketches/shaders/mask_pixelator.frag', { varyings: Tree.texcoords2 });
-  // image source: https://en.wikipedia.org/wiki/HSL_and_HSV#/media/File:Fire_breathing_2_Luc_Viatour.jpg
+  pixelatorShader = readShader('/VisualComputing/sketches/shaders/pixelator/mask.frag', { varyings: Tree.texcoords2 });
   img = loadImage('/VisualComputing/sketches/shaders/images/lion.jpg');
 }
 
 function setup() {
-  createCanvas(290, 290, WEBGL);
+  createCanvas(640, 500, WEBGL);
   noStroke();
   textureMode(NORMAL);
-  shader(lumaShader);
-  lumaShader.setUniform('texture', img);
+  shader(pixelatorShader);
+  pixelatorShader.setUniform('texture', img);
+
+  size = createSlider(1, 500, 100, 5);
+  size.style('width', '500px');
+  size.position(70, 490);
 }
 
 function draw() {
   background(0);
+  pixelatorShader.setUniform('size', size.value());
   quad(-width / 2, -height / 2, width / 2, -height / 2, width / 2, height / 2, -width / 2, height / 2);
 }
